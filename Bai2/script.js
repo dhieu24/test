3e15ce99e370820c6a48c8c6c969d84e12d24284
DESKTOP-E09HDCR\Admin <hieudangk27@gmail.com>
2021-01-06 14:44:57 +0700

    commit

--- Bai2/script.js
+++ Bai2/script.js
@@ -18,19 +18,36 @@ var footballClub = [{
         points : 88,
         GD: 39
     },
+    {
+        name: "A",
+        points : 88,
+        GD: 39
+    }
     
 ];
 
+// trả về array đội bóng không lặp
+var arr = [];
+var arr2 = []
 
-let arr = [];
 for(let i =0; i < footballClub.length; i++) {
     arr.push(footballClub[i].name);
 }
-var repeatName = arr.filter((item, index) => arr.indexOf(item) !== index);
+console.log(arr);
 
-for(let i =0; i < footballClub.length; i++) {
-    if(arr[i] === repeatName) {
-        var index = arr.indexOf(repeatName);
-        arr.splice(index, 1);
+var repeatName = arr.filter((item, index) => arr.indexOf(item) !== index);
+for(let i =0; i < arr.length; i++) {
+    if(repeatName.includes(arr[i]) == false) {
+        arr2.push(arr[i]);
     }
 }
+var arrFinal = arr2.concat(repeatName);
+console.log(arrFinal);
+
+// sắp xếp các câu lạc bộ theo điểm
+footballClub.sort(function (club1, club2) {
+    return club1.points - club2.points;
+});
+console.log(footballClub);
+
+
